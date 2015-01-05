411f122b377a4c4530e3b4d3b5d3f1fb68f1ed1b
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-01-05 10:17:31 +0100

    Added the following examples: 021-022.

---
+++ 021/Math1.java
@@ -0,0 +1,10 @@
+class Math1 {
+    public static void main(String[] args) {
+        double TWO_PI = Math.PI * 2.0;
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(TWO_PI);
+    }
+}
+
---
+++ 022/Math2.java
@@ -0,0 +1,11 @@
+class Math2 {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-10));
+
+        System.out.println(Math.sin(Math.PI/2.0));
+        System.out.println(Math.cos(Math.PI/3.0));
+
+        System.out.println(Math.hypot(3, 4));
+    }
+}
+
